1139cb358d9a01e4af8058d18742a6ceabbd0109
LoginamNet <loginamnet@gmail.com>
2022-06-23 09:35:00 +0300

    refactor: add more redability

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  
   let result = [];
 
     if (matrix) {  
       for (let i = 0; i < matrix.length; i++) {
-        if (i % 2 !== 0) {matrix[i].sort((a, b) => b - a)}
+        if (i % 2 !== 0) {
+          matrix[i].sort((a, b) => b - a);
+        }
         result = result.concat(matrix[i]);
       }
-  
-
-}
+    }
   return result;
 }
