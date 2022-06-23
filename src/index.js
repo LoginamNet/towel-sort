050d2c72e6b7fc4d68541fb9fadd0585454bb389
LoginamNet <loginamnet@gmail.com>
2022-06-23 09:32:47 +0300

    fix: fix empty arr & non parameter errors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  for (let i = 0; i < matrix.length; i++) {
-
-    if (i % 2 !== 0) {matrix[i].sort((a, b) => b - a)}
-
-    console.log(matrix);
-  }
   
-  return matrix.reduce((a, b) => a.concat(b));
+  let result = [];
+
+    if (matrix) {  
+      for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 !== 0) {matrix[i].sort((a, b) => b - a)}
+        result = result.concat(matrix[i]);
+      }
+  
+
+}
+  return result;
 }
